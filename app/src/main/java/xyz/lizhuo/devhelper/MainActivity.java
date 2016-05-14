e7863a05706ad27773c01cf96c48db4413f71d18
huanglizhuo <huanglizhuo1991@gmail.com>
2016-05-14 17:14:29 +0800

    change short screen on time to 60m

--- app/src/main/java/xyz/lizhuo/devhelper/MainActivity.java
+++ app/src/main/java/xyz/lizhuo/devhelper/MainActivity.java
@@ -5,89 +5,172 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final int REQUEST_CODE_WRITE_SETTINGS = 2;
-  private int screenTimeOut = 15000;
-  private Utils utils;
+    private static final String TAG = "ServerThread";
+    private static final int REQUEST_CODE_WRITE_SETTINGS = 2;
+    private int screenTimeOut = 15000;
+    private Utils utils;
 
-  @Bind(R.id.adbWifiEnable) TextView wifi;
+    public String s;
+    @Bind(R.id.adbWifiEnable)
+    TextView wifi;
+    @Bind(R.id.socket)
+    TextView socket;
+    ServerThread serverThread;
+    Handler handler = new Handler() {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-    utils = new Utils();
-  }
-
-  @TargetApi(Build.VERSION_CODES.M) @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == REQUEST_CODE_WRITE_SETTINGS) {
-      if (Settings.System.canWrite(this)) {
-        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
-            screenTimeOut);
-      } else {
-
-      }
-    }
-  }
-
-  @TargetApi(Build.VERSION_CODES.M) @OnClick({
-      R.id.adbWifiEnable, R.id.debugLayoutEnable, R.id.debugLayoutDisable,
-      R.id.showGPUBarEnable, R.id.showGPUBarDisable, R.id.setNoClose, R.id.nonal
-  }) public void onClick(View view) {
-    switch (view.getId()) {
-      case R.id.adbWifiEnable:
-        utils.startWifiAdb(true);
-        wifi.setText("ip address : " + utils.getIpAddress(this));
-        break;
-      case R.id.debugLayoutEnable:
-        utils.setDebugLayout(true);
-        break;
-      case R.id.debugLayoutDisable:
-        utils.setDebugLayout(false);
-        break;
-      case R.id.showGPUBarEnable:
-        utils.setGPULineBar(true);
-        break;
-      case R.id.showGPUBarDisable:
-        utils.setGPULineBar(false);
-        break;
-      case R.id.overDraw:
-        utils.setOverDraw(true);
-        break;
-      case R.id.setNoClose: {
-        if (!Settings.System.canWrite(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
-                Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
-          }else{
-            screenTimeOut=2147483647;
-            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
-              screenTimeOut);
-          }
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(getApplicationContext(), msg.getData().getString("MSG", "Toast"), Toast.LENGTH_SHORT).show();
+            s += msg.getData().getString("MSG", "Toast");
+            socket.setText(s);
         }
-        break;
-      case R.id.nonal:{
-        if (!Settings.System.canWrite(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
-                Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
-          }else{
-            screenTimeOut=15000;
-            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
-              screenTimeOut);
-          }
-        }
-        break;
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        utils = new Utils();
+        serverThread = new ServerThread();
+        serverThread.start();
     }
-  }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_WRITE_SETTINGS) {
+            if (Settings.System.canWrite(this)) {
+                Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
+                        screenTimeOut);
+            } else {
+
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @OnClick({
+            R.id.adbWifiEnable, R.id.debugLayoutEnable, R.id.debugLayoutDisable,
+            R.id.showGPUBarEnable, R.id.showGPUBarDisable, R.id.setNoClose, R.id.nonal
+    })
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.adbWifiEnable:
+                utils.startWifiAdb(true);
+                wifi.setText("ip address : " + utils.getIpAddress(this));
+                break;
+            case R.id.debugLayoutEnable:
+                utils.setDebugLayout(true);
+                break;
+            case R.id.debugLayoutDisable:
+                utils.setDebugLayout(false);
+                break;
+            case R.id.showGPUBarEnable:
+                utils.setGPULineBar(true);
+                break;
+            case R.id.showGPUBarDisable:
+                utils.setGPULineBar(false);
+                break;
+            case R.id.overDraw:
+                utils.setOverDraw(true);
+                break;
+            case R.id.setNoClose: {
+                if (!Settings.System.canWrite(this)) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
+                } else {
+                    screenTimeOut = 2147483647;
+                    Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
+                            screenTimeOut);
+                }
+            }
+            break;
+            case R.id.nonal: {
+                if (!Settings.System.canWrite(this)) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
+                } else {
+                    screenTimeOut = 60000;
+                    Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT,
+                            screenTimeOut);
+                }
+            }
+            break;
+        }
+    }
+
+    class ServerThread extends Thread {
+
+        boolean isLoop = true;
+
+        public void setIsLoop(boolean isLoop) {
+            this.isLoop = isLoop;
+        }
+
+        @Override
+        public void run() {
+            Log.d(TAG, "running");
+
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(9000);
+
+                while (isLoop) {
+                    Socket socket = serverSocket.accept();
+
+                    Log.d(TAG, "accept");
+
+                    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+                    String msg = inputStream.readUTF();
+
+                    Message message = Message.obtain();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("MSG", msg);
+                    message.setData(bundle);
+                    handler.sendMessage(message);
+
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                Log.d(TAG, "destory");
+
+                if (serverSocket != null) {
+                    try {
+                        serverSocket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/xyz/lizhuo/devhelper/Utils.java
+++ app/src/main/java/xyz/lizhuo/devhelper/Utils.java
@@ -1,13 +1,10 @@
 package xyz.lizhuo.devhelper;
 
 import android.content.Context;
-import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.util.Log;
+
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,65 +15,67 @@ import java.io.InputStreamReader;
  */
 public class Utils {
 
-  private volatile static  Process process=null;
+    private volatile static Process process = null;
 
-  public Utils(){
-    try {
-      process = Runtime.getRuntime().exec("su\n");
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public  String  getIpAddress(Context context){
-    int ipAddress=0;
-    WifiManager wifiManager = (WifiManager) context.getSystemService(android.content.Context.WIFI_SERVICE );
-    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-    ipAddress = wifiInfo.getIpAddress();
-    return (ipAddress & 0xFF ) + "." +
-        ((ipAddress >> 8 ) & 0xFF) + "." +
-        ((ipAddress >> 16 ) & 0xFF) + "." +
-        ( ipAddress >> 24 & 0xFF) ;
-  }
-
-  public  void startWifiAdb(boolean enable){
-    execShell(new String[]{"setprop service.adb.tcp.port "+(enable? "5555":"-1 \n" ),"stop adbd\n","start adbd\n"});
-  }
-
-  public  void setDebugLayout(boolean show){
-    execShell(new String[]{"setprop debug.layout "+(show ? "true ":"false")+ "\n"});
-  }
-
-  public  void setOverDraw(boolean show){
-    execShell(new String[]{"setprop debug.hwui.overdraw"+(show ? "show ":"false")+ "\n"});
-  }
-
-
-
-  public  void setGPULineBar(boolean show){
-      execShell(new String[] { "setprop debug.hwui.profile "+(show ? "visual_bars ":"false")+ "\n"});
-  }
-
-  private void execShell(String[] commands) {
-    //Process process = null;
-    try {
-      //process = Runtime.getRuntime().exec("su\n");
-
-      DataOutputStream os = new DataOutputStream(process.getOutputStream());
-      for (String command : commands) {
-          if (command == null) {
-            continue;
-          }
-        os.write(command.getBytes());
-        os.writeBytes("\n");
-      }
-      //os.writeBytes("exit\n");
-      os.flush();
-      os.close();
-      //process.destroy();
-    } catch (IOException e) {
-      e.printStackTrace();
+    public Utils() {
+        try {
+            process = Runtime.getRuntime().exec("su\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-  }
+    public String getIpAddress(Context context) {
+        int ipAddress = 0;
+        WifiManager wifiManager = (WifiManager) context.getSystemService(android.content.Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        ipAddress = wifiInfo.getIpAddress();
+        return (ipAddress & 0xFF) + "." +
+                ((ipAddress >> 8) & 0xFF) + "." +
+                ((ipAddress >> 16) & 0xFF) + "." +
+                (ipAddress >> 24 & 0xFF);
+    }
+
+    public void startWifiAdb(boolean enable) {
+        execShell(new String[]{"setprop service.adb.tcp.port " + (enable ? "5555" : "-1 \n"), "stop adbd\n", "start adbd\n"});
+    }
+
+    public void setDebugLayout(boolean show) {
+        execShell(new String[]{"setprop debug.layout " + (show ? "true " : "false") + "\n"});
+    }
+
+    public void setOverDraw(boolean show) {
+//    execShell(new String[]{"setprop debug.hwui.overdraw "+(show ? "show ":"false")+ "\n"});
+        execShell(new String[]{"setprop debug.hwui.overdraw show \n"});
+    }
+
+
+    public void setGPULineBar(boolean show) {
+        execShell(new String[]{"setprop debug.hwui.profile " + (show ? "visual_bars " : "false") + "\n"});
+    }
+
+    private void execShell(String[] commands) {
+        try {
+            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+            InputStream is = process.getInputStream();
+            for (String command : commands) {
+                if (command == null) {
+                    continue;
+                }
+                os.write(command.getBytes());
+                os.write("ls".getBytes());
+                os.writeBytes("\n");
+            }
+            //os.writeBytes("exit\n");
+            os.flush();
+            os.close();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+            String line =null;
+            line = buf.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
